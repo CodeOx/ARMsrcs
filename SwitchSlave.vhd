f0d8fa61be8c727cd4cd878039b7dce97722e8ca
djin31 <deepanshujindal.99@gmail.com>
2018-04-18 15:43:28 +0530

    switchslave added

---
+++ SwitchSlave.vhd
@@ -0,0 +1,41 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity SwitchSlave is
+   Port(Hsel : in STD_LOGIC;
+        Haddress : in STD_LOGIC_VECTOR(15 downto 0);
+        HWrite : in STD_LOGIC;
+        HReady : out STD_LOGIC;
+        HTrans : in STD_LOGIC_VECTOR(1 downto 0);        
+        HReset : in STD_LOGIC;
+        HClock : in STD_LOGIC;
+        HRData : out STD_LOGIC_VECTOR(31 downto 0);
+        Switches : in STD_LOGIC_VECTOR(31 downto 0));
+end SwitchSlave;
+
+architecture Behavioral of MemorySlave is
+
+    Type State is (
+        IDLE,
+        EXECUTE
+    );
+begin
+
+    process(HClock,HReset)
+    begin
+        if HReset = '1' then 
+            curr_state<=IDLE;
+        elsif rising_edge(HClock) then
+            --case curr_state is
+            --when IDLE =>
+                if HTrans = "10" and HSel ='1' and HWrite = '0' then
+                --    curr_state <= EXECUTE;
+                    HRData <= Switches;
+                    HReady <= '1';                    
+                end if;
+            --when EXECUTE =>
+              --  curr_state <= IDLE;
+        end if;
+
+    end process;
+end Behavioral;
